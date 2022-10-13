cc8ae84bd741a5b78e4226430c63644e67af1475
C-Macargo <caio.camargos97@gmail.com>
2022-10-13 17:03:38 -0300

    Layout com seleçao de itens

---
+++ script.js
@@ -0,0 +1,94 @@
+// escolher Primeiro prato script//
+function EscolherFrango(){  
+    document.getElementById("Frango").style.borderColor = "green";
+    document.getElementById("Pratof").style.borderColor = "white";
+    document.getElementById("Macarrao").style.borderColor = "white";
+    document.getElementById("Salada").style.borderColor = "white";
+
+}
+
+function EscolherPf(){
+    document.getElementById("Frango").style.borderColor = "white";
+    document.getElementById("Pratof").style.borderColor = "green";
+    document.getElementById("Macarrao").style.borderColor = "white";
+    document.getElementById("Salada").style.borderColor = "white";
+}
+
+function EscolherMacarrao(){
+    document.getElementById("Frango").style.borderColor = "white";
+    document.getElementById("Pratof").style.borderColor = "white";
+    document.getElementById("Macarrao").style.borderColor = "green";
+    document.getElementById("Salada").style.borderColor = "white";
+
+    
+}
+
+function EscolherSalada(){
+    document.getElementById("Frango").style.borderColor = "white";
+    document.getElementById("Pratof").style.borderColor = "white";
+    document.getElementById("Macarrao").style.borderColor = "white";
+    document.getElementById("Salada").style.borderColor = "green";
+}
+
+// escolher Bebida script//
+function EscolherCoca(){  
+    document.getElementById("Coca").style.borderColor = "green";
+    document.getElementById("Suco").style.borderColor = "white";
+    document.getElementById("Cha").style.borderColor = "white";
+    document.getElementById("Agua").style.borderColor = "white";
+}
+
+function EscolherSuco(){  
+    document.getElementById("Coca").style.borderColor = "white";
+    document.getElementById("Suco").style.borderColor = "green";
+    document.getElementById("Cha").style.borderColor = "white";
+    document.getElementById("Agua").style.borderColor = "white";
+}
+
+function EscolherCha(){  
+    document.getElementById("Coca").style.borderColor = "white";
+    document.getElementById("Suco").style.borderColor = "white";
+    document.getElementById("Cha").style.borderColor = "green";
+    document.getElementById("Agua").style.borderColor = "white";
+}
+
+function EscolherAgua(){  
+    document.getElementById("Coca").style.borderColor = "white";
+    document.getElementById("Suco").style.borderColor = "white";
+    document.getElementById("Cha").style.borderColor = "white";
+    document.getElementById("Agua").style.borderColor = "green";
+}
+
+
+//escolher sobremesa script//
+function EscolherPudim(){  
+    document.getElementById("Pudim").style.borderColor = "green";
+    document.getElementById("Brownie").style.borderColor = "white";
+    document.getElementById("Quindim").style.borderColor = "white";
+    document.getElementById("Brigadeiro").style.borderColor = "white";
+    
+}
+
+function EscolherBrownie(){  
+    document.getElementById("Pudim").style.borderColor = "white";
+    document.getElementById("Brownie").style.borderColor = "green";
+    document.getElementById("Quindim").style.borderColor = "white";
+    document.getElementById("Brigadeiro").style.borderColor = "white";
+    
+}
+
+function EscolherQuindim(){  
+    document.getElementById("Pudim").style.borderColor = "white";
+    document.getElementById("Brownie").style.borderColor = "white";
+    document.getElementById("Quindim").style.borderColor = "green";
+    document.getElementById("Brigadeiro").style.borderColor = "white";
+    
+}
+
+function EscolherBrigadeiro(){  
+    document.getElementById("Pudim").style.borderColor = "white";
+    document.getElementById("Brownie").style.borderColor = "white";
+    document.getElementById("Quindim").style.borderColor = "white";
+    document.getElementById("Brigadeiro").style.borderColor = "green";
+    
+}
